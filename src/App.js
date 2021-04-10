f5c4a3db2f63a658e2049df35bf263d4f4cabd28
Esmerlin79 <68618650+Esmerlin79@users.noreply.github.com>
2021-04-10 19:40:40 -0400

    Add Header and component products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Components/Header';
+import Products from './Components/Products';
+import NewProduct from './Components/NewProduct';
+import EditProduct from './Components/EditProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Header />
+          <div className="container mt-5">
+              <switch>
+                  <Route exact path="/" component={Products} />
+                  <Route exact path="/products/new" component={NewProduct} />
+                  <Route exact path="/products/edit/:id" component={EditProduct} />
+              </switch>
+          </div>
+      </Router>
   );
 }
 
---
+++ src/Components/NewProduct.js
@@ -0,0 +1,9 @@
+import React from 'react';  
+
+const NewProduct = () => {
+    return ( 
+        <h1>Product new</h1>
+     );
+}
+ 
+export default NewProduct;
