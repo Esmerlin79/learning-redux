d768eeec3cb228b1fbf7a3f3f3fe97cb304bdd4f
Esmerlin79 <68618650+Esmerlin79@users.noreply.github.com>
2021-04-10 22:49:18 -0400

    add configuration redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Components/Header';
 import Products from './Components/Products';
 import NewProduct from './Components/NewProduct';
 import EditProduct from './Components/EditProduct';
 
+//REDUX 
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
       <Router>
-          <Header />
-          <div className="container mt-5">
-              <switch>
-                  <Route exact path="/" component={Products} />
-                  <Route exact path="/products/new" component={NewProduct} />
-                  <Route exact path="/products/edit/:id" component={EditProduct} />
-              </switch>
-          </div>
-      </Router>
-  );
+          <Provider store={store}>
+            <Header />
+            <div className="container mt-5">
+                <Switch>
+                    <Route exact path="/" component={Products} />
+                    <Route exact path="/products/new" component={NewProduct} />
+                    <Route exact path="/products/edit/:id" component={EditProduct} />
+                </Switch>
+            </div>
+          </Provider>
+    </Router>
+);
 }
-
 export default App;
